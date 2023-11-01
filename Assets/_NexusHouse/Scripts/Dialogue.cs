d9566419e06c2d744d42eab6175636c3a5522838
Kristine Jimenez <k.jimenez@student.saedubai.com>
2023-11-01 11:30:48 +0400

    colliders, layers, movement fixes

--- Assets/_NexusHouse/Scripts/Dialogue.cs
+++ Assets/_NexusHouse/Scripts/Dialogue.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Dialogue : MonoBehaviour
 {
     public AudioSource SBVoice1Source;
+    public BoxCollider SBVoice1Collider;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             SBVoice1Source.Play();
+            Destroy(SBVoice1Collider);
         }
     }
 }
