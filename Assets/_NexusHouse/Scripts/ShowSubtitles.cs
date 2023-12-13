1d2aa9dafe952fb77612c680ff328980a4a58d1a
Kristine Jimenez <k.jimenez@student.saedubai.com>
2023-12-13 11:08:29 +0400

    occlusion settings edited + subtitles edits

--- Assets/_NexusHouse/Scripts/ShowSubtitles.cs
+++ Assets/_NexusHouse/Scripts/ShowSubtitles.cs
@@ -22,9 +22,9 @@ public class ShowSubtitles : MonoBehaviour
     public async void InterfaceOneSubtitles()
     {
         currentSubtitle.enabled = true;
-        currentSubtitle.text = "Target destination reached. Please be mindful of any interactions with the past,";
+        currentSubtitle.text = "INTERFAACE: Target destination reached. Please be mindful of any interactions with the past,";
         await Task.Delay(5 * 1000);
-        currentSubtitle.text = "for your safety, and for the safety of the timeline.";
+        currentSubtitle.text = "INTERFACE: for your safety, and for the safety of the timeline.";
         await Task.Delay(4 * 1000);
         currentSubtitle.enabled = false;
     }
@@ -32,11 +32,11 @@ public class ShowSubtitles : MonoBehaviour
     public async void SBVoice1Subtitles()
     {
         currentSubtitle.enabled = true;
-        currentSubtitle.text = "Hmm… This looks like the right place but…";
+        currentSubtitle.text = "SPECTER: Hmm… This looks like the right place but…";
         await Task.Delay(2 * 1000);
-        currentSubtitle.text = " It looks different from what they showed me in the briefing.";
+        currentSubtitle.text = "SPECTER: It looks different from what they showed me in the briefing.";
         await Task.Delay(3 * 1000);
-        currentSubtitle.text = "Looks… older. I should try to look for something to confirm where I am.";
+        currentSubtitle.text = "SPECTER: Looks… older. I should try to look for something to confirm where I am.";
         await Task.Delay(4 * 1000);
         currentSubtitle.enabled = false;
     }
