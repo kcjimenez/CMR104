8b5dbb31fa97bd14cccf2e95192d5bdc74f36664
Kristine Jimenez <k.jimenez@student.saedubai.com>
2023-12-20 11:26:50 +0400

    stop movement on dialogue

--- Assets/_NexusHouse/Scripts/OneTimeCalled.cs
+++ Assets/_NexusHouse/Scripts/OneTimeCalled.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class OneTimeCalled : MonoBehaviour
 {
     public UnityEvent onCalled;
+    public UnityEvent onFinished;
     bool hasBeenCalled = false;
-    public void CallOnce()
+    public int waitTime;
+
+    public async void CallOnce()
     {
         if(!hasBeenCalled)
         {
             onCalled.Invoke();
             hasBeenCalled = true;
+            await Task.Delay(waitTime * 1000);
+            onFinished.Invoke();
         }
     }
 }
